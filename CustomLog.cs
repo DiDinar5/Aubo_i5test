61b3b7b581f5e62e01581fe446730ca2115bee69
Dinar <dinar-wp9@mail.ru>
2023-07-16 23:08:58 +0300

    Add project files.

---
+++ CustomLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveObjectWithMouse
+{
+    public  class CustomLog
+    {
+        public static void Log(string message)
+        {
+            File.AppendAllLines($"log.txt",new List<string>{$"{DateTime.Now}: {message}"});
+        }
+    }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MoveObjectWithMouse
+{
+    public partial class MainWindow : Window
+    {
+        Point oldPosition = new Point();
+        public MainWindow()
+        {   
+            InitializeComponent();
+            RobotAubo.RunRobot();
+        }
+
+        private void MouseMoveHandler(object sender, MouseEventArgs e)
+        {
+            // Get the x and y coordinates of the mouse pointer.
+            var position = e.GetPosition(this);
+            
+            var pX = position.X;
+            var pY = position.Y;
+
+            // Sets the Height/Width of the circle to the mouse coordinates.
+            ellipse.Width = pX;
+            ellipse.Height = pY;
+            //position текущая, oldPosition предыдущая
+            RobotAubo.relativeOffsetExample(position.X - oldPosition.X, position.Y - oldPosition.Y);
+            oldPosition = position;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RobotAubo.StopRobot();
+            this.Close();
+        }
+    }
+}
